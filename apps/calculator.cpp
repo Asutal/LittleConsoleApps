375a1148f76456882a69d457f7395e7561053fcb
Asutal <78130185+Asutal@users.noreply.github.com>
2021-01-29 09:33:00 +0800

    Update calculator.cpp

--- apps/calculator.cpp
+++ apps/calculator.cpp
@@ -1,1 +1,47 @@
+/*********************
+ * coding: UTF-8     *
+ * Auther: Asutal    *
+ *********************/
+#include <iostream>
+using namespace std;
 
+double jisuan(double a, char oper, double c)
+{
+    switch (oper)
+    {
+    case '+':
+        return a + c;
+        break;
+    case '-':
+        return a - c;
+        break;
+    case '*':
+        return a * c;
+        break;
+    case '/':
+        return ( c ? a / c : 0);
+        //三目运算符 c为0返回0
+        break;
+    default:
+        cerr << "Error\n";
+        break;
+    }
+}
+
+int main()
+{
+    double num1 = 0;
+    double num2 = 0;
+    char oper = 0;
+    char choo = 0;
+    do
+    {
+        cout << "请输入数据" << endl;
+        cout << "例：a(+,-,*,/)b" <<endl;
+        cin >> num1 >> oper >> num2;
+        cout << jisuan(num1, oper, num2) << endl;
+        cout << "Continue?(y/n)";
+        cin >> choo;
+    } while (choo == 'Y'||'y');
+    return 0;
+}
